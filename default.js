602103e203f7db9ca2915636f982076a339aa639
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 13:01:05 +0600

    by Default peramiter value  set in function

--- default.js
+++ default.js
@@ -1,5 +1,5 @@
-function addtoNumber(num1,num2){
-    num2 = num2 || 0
+function addtoNumber(num1,num2 = 10){ //by default num2 set is 10 if we miss pass any number when call this function
+        // num2 = num2 || 0
     return num1+num2
 }
 
