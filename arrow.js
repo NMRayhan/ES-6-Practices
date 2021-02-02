73555a89c93ead949f223aa2e224e1b30f11eef8
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 22:09:02 +0600

    ES-6 New Way Function Declaration Call Arrow function

---
+++ arrow.js
@@ -0,0 +1,12 @@
+const doubleIt = num => num*2
+
+// const doubleIt = function MyNumber(num){
+//     return num*2
+// }
+
+// function doubleIt(num){
+//     return num*2
+// }
+
+const result = doubleIt(9)
+console.log(result);
