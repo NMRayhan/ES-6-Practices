d894f376779077cbcb274a22efe2c57f6b164ee1
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 13:02:43 +0600

    another way missing argument puss

--- default.js
+++ default.js
@@ -3,5 +3,5 @@ function addtoNumber(num1,num2 = 10){ //by default num2 set is 10 if we miss pas
     return num1+num2
 }
 
-const result = addtoNumber(11)
+const result = addtoNumber(11,1) //in this Argument if we pass any Value then this Argument value is set in num2
 console.log(result);
