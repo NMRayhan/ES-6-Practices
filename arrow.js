74a37a1f45bce7b9c4afc12dda9a71b2db3c1407
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 22:37:14 +0600

    Arrow function

--- arrow.js
+++ arrow.js
@@ -1,4 +1,20 @@
-const doubleIt = num => num*2
+const CheckNumber = (num1) => {
+    if(num1%2==0){
+        return `${num1} is Even Number`;
+    }else{
+        return `${num1} is Odd Number`
+    }
+}
+
+const result1 = CheckNumber(3);
+console.log(result1);
+
+
+const giveMe5 = () => 5;
+const result3 = giveMe5();
+console.log(result3);
+
+// const doubleIt = num => num*2
 
 // const doubleIt = function MyNumber(num){
 //     return num*2
@@ -8,5 +24,7 @@ const doubleIt = num => num*2
 //     return num*2
 // }
 
-const result = doubleIt(9)
+const doubleIt = num1 => (num1>10) ? 'Greater Then 10' : 'Less then 10'
+
+const result = doubleIt(10)
 console.log(result);
