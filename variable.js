9f59cc81823555d61f3d3f654e211030c6945588
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 12:37:01 +0600

    Object defined in const Variable

--- variable.js
+++ variable.js
@@ -1,12 +1,12 @@
-const number = [12,23,44,56,64,32]
-console.log(number);
-number[2] = 10; //number[2] = previous 44 and Now 10
-console.log(number); 
+const person = {
+    name : "Rayhan",
+    age : 24,
+    skill : ["HTML","CSS","Database","C Programming", "Java", "Javascript","Nodejs","React-js"],
+}
 
-number.push(11); //number are new element push
-console.log(number);
-
-number.pop() //number last element are pop
-console.log(number);
-
-number= [1,2,3,4,5] //this is not true and Not Happened because of number variable ar Constant
+console.log(person.skill);
+person.skill.push("python")
+console.log(person.skill);
+person = { // Qualification is not defined
+    Qualification = "BSC in SWE"
+}
