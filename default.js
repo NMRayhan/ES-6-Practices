eee82d1aabada11e766d2e490c710690496f8e10
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 12:51:35 +0600

    if we missed any number pass as Argument

--- default.js
+++ default.js
@@ -1,6 +1,9 @@
 function addtoNumber(num1,num2){
+    if(num2 == undefined){
+        num2 = 0;
+    }
     return num1+num2
 }
 
-const result = addtoNumber(11,23)
+const result = addtoNumber(11)
 console.log(result);
