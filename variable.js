36cc1aefecc138877c1cfefd498f196ec9836090
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 12:25:46 +0600

    constant Array variable set

--- variable.js
+++ variable.js
@@ -1,5 +1,12 @@
-function hubby(name){
-    const hubbyName = name
-    console.log(hubbyName);
-}
-hubby("Elias Kanchon")
+const number = [12,23,44,56,64,32]
+console.log(number);
+number[2] = 10; //number[2] = previous 44 and Now 10
+console.log(number); 
+
+number.push(11); //number are new element push
+console.log(number);
+
+number.pop() //number last element are pop
+console.log(number);
+
+number= [1,2,3,4,5] //this is not true and Not Happened because of number variable ar Constant
