190c1a87a443a7b1f1f3bdb34b4c3c2e85a5ce1a
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 16:15:34 +0600

    Diffrent Way String Concationation and Multiple line print

--- default.js
+++ default.js
@@ -1,7 +1,4 @@
-function addtoNumber(num1,num2 = 10){ //by default num2 set is 10 if we miss pass any number when call this function
-        // num2 = num2 || 0
-    return num1+num2
-}
+const name1 = "Nur Mohammad ", name2 = "Rayhan"
 
-const result = addtoNumber(11,1) //in this Argument if we pass any Value then this Argument value is set in num2
-console.log(result);
+const fullName = name1+name2+" I am Honest Person, I love Programming"
+console.log(fullName);
