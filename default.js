7fdb132fc6523e6dea1cd06dc1005dbc90bf20e4
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-02 16:23:12 +0600

    Template in ES6

--- default.js
+++ default.js
@@ -1,4 +1,13 @@
 const name1 = "Nur Mohammad ", name2 = "Rayhan"
 
 const fullName = name1+name2+" I am Honest Person, I love Programming"
-console.log(fullName);
+const fullName2 =  `${name1} ${name2} I love Programming. I Live in Dhaka`
+const multiline = "I Love You " +
+"I need You " +
+"I Proud of You"
+const multiline2 = `Hello Every one
+How are You all
+We are Happy to see you`
+console.log(fullName2);
+console.log(multiline);
+console.log(multiline2);
